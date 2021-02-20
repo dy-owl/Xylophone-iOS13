3476a51dc9dab7f12fd2cb9ea310c9c17e845528
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-02-20 19:28:54 +0200

    Buttons click now have an animation.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: (sender.titleLabel?.text)!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200)) {
+            sender.alpha = 1.0
+        }
     }
 
     func playSound(soundName: String) {
